00e7dddd80ad1d73526f390858b7710c7db1c1af
Samrat Ghosh <samratgh2004@gmail.com>
2025-08-25 00:50:06 +0530

    Add files via upload

---
+++ First APP/MyFirstAPP/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.logging.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+       // AutoCompleteTextView
+
+    }
+}
